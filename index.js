164d0aff4cbf212eabef48feafbcd956173dfb2b
kev <45981465+abbo-1@users.noreply.github.com>
2020-02-25 15:48:39 -0600

    almost done

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function clock {
-    
-}
+const fullDate = new Date();
+const hours = fullDate.getHours();
+const mins = fullDate.getMinutes();
+const secs = fullDate.getSeconds();
+
+document.getElementById('hours')
+document.getElementById('minutes')
+document.getElementById('seconds')
